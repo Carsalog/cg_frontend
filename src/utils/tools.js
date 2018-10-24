d1cba556a5ee6550a6ca976c4edf52f62ee5deeb
DudkinON <dudkin.on@gmail.com>
2018-10-23 23:10:35 -0500

    docs: Add a docstring to decodeUser function

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -2,6 +2,11 @@ import JwtDecode from "jwt-decode";
 
 
 export const decodeUser = token => {
+  /**
+   * Try to decode user, return user object or null
+   * @param token: String
+   * @return Object|null:
+   */
 
   try {
     return JwtDecode(token);
