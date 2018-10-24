6136aafd8b77b0d8898f453e1355bf6d73fabf01
DudkinON <dudkin.on@gmail.com>
2018-10-24 01:00:35 -0500

    refactor: Update logo

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,7 +4,10 @@ import {Link} from 'react-router-dom';
 
 const Nav = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
-    <Link className="navbar-brand h4" to="/"><span style={{color: "#007bff"}}>C</span>arsalog</Link>
+    <Link className="navbar-brand h4" to="/" id="logo">
+      <span style={{color: "#007bff"}}>C</span>
+      <span>arsalog</span>
+    </Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"> </span>
