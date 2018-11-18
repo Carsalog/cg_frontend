ad994534d3eea12c530b6e0f46edafb5b89f50ae
DudkinON <dudkin.on@gmail.com>
2018-11-17 23:06:44 -0600

    feat: Add Loguot component

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Redirect, Route, Switch} from "react-router-dom";
 import ProtectedRoute from "./components/common/ProtectedRoute";
 import Login from "./components/Login";
+import Logout from "./components/Logout";
 import NotFound from "./components/404";
-import Base from "./components/base";
+import Base from "./components/Base";
 
 const Router = () => (
   <Switch>
     <Route path="/login" component={Login}/>
+    <Route path="/logout" component={Logout}/>
     <Route path="/not-found" component={NotFound}/>
     <Route path="/" exact component={Base}/>
     <Redirect from="*" to="/not-found"/>
