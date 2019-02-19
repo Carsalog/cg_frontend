ef2baf8a5e5cdaa4c6c9320f32af31103ff886b9
DudkinON <dudkin.on@gmail.com>
2019-02-19 15:35:08 -0800

    refactor: Change action type in model reducer

--- src/reducers/model.js
+++ src/reducers/model.js
@@ -2,7 +2,7 @@ import {SET_MODEL} from "../actions/types";
 
 
 const model = (state = null, action) => {
-  if (action.type === 'SET_MODEL') return action.payload;
+  if (action.type === SET_MODEL) return action.payload;
   return state
 };
 
