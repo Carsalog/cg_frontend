0eee2a127ccfc885c27b80bae04fee0c28830f62
DudkinON <dudkin.on@gmail.com>
2019-03-06 13:07:38 -0800

    refactor: Move code to Navlink component

--- src/components/common/AdminButton.jsx
+++ src/components/common/AdminButton.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import NavLink from './NavLink';
 
 
-export default su => su ? (
-  <Link className="nav-item nav-link" to="/settings">
-    <i className="fa fa-cog" aria-hidden="true"> </i>
-  </Link>
-) : <i/>;
+export default ({su}) => su ? <NavLink
+  path="/settings"
+  iconStyle="fa fa-cog"
+  text="Admin panel"
+  textStyle="icon-text pr-2"/> : null;
 
